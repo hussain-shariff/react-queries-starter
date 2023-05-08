a19cb2d93f9f111aa6442b9600b18e927503ff50
hussain-shariff <hussainowaiz070@gmail.com>
2023-05-08 15:28:30 +0530

    api

--- src/components/SuperHeroes.page.tsx
+++ src/components/SuperHeroes.page.tsx
@@ -1,7 +1,27 @@
+import {useState, useEffect} from "react"
 
 const SuperHeroesPage = () => {
+  const [data, setdata] = useState<any>(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res=> res.json())
+      .then(json=> {
+        setdata(json)
+        setIsLoading(false)
+      })
+  }, [])
+
+  if(isLoading) return <h1>Loading...</h1>
+
   return (
-    <div>SuperHeroes.page</div>
+    <div className=" p-10">
+      <p>SuperHeroes Page</p>
+      { data.map((user)=>(
+        <h1 key={user.id}>{user.name}</h1>
+      ))}
+    </div>
   )
 }
 
