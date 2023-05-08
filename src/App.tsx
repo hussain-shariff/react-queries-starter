0e4e16473ff8e59d2edb6294e97743b8f0499d2b
hussain-shariff <hussainowaiz070@gmail.com>
2023-05-08 15:32:09 +0530

    react-query setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { Route, BrowserRouter as Router, Routes, Link } from "react-router-dom"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import { QueryClientProvider, QueryClient } from "react-query" 
 import HomePage from "./components/Home.page"
 import RQsuperHeroesPage from "./components/RQsuperHeroes.page"
 import SuperHeroesPage from "./components/SuperHeroes.page"
 import Nav from "./components/Nav"
 
+const queryClient = new QueryClient()
+
 function App() {
   
   return (
-      <Router>
+    <QueryClientProvider client={queryClient}>
+        <Router>
           <Nav/>
-        <Routes>
-          <Route path="/home" element={<HomePage/>}></Route>
-          <Route path="/superHeroes" element={<SuperHeroesPage/>}></Route>
-          <Route path="/RQsuperHeroes" element={<RQsuperHeroesPage/>}></Route>
-        </Routes>
-      </Router>
+          <Routes>
+            <Route path="/home" element={<HomePage/>}></Route>
+            <Route path="/superHeroes" element={<SuperHeroesPage/>}></Route>
+            <Route path="/RQsuperHeroes" element={<RQsuperHeroesPage/>}></Route>
+          </Routes>
+        </Router>
+    </QueryClientProvider>
+      
   )
 }
 
